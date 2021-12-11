7f732b2fe23ba22ae2900261b3ebbdf5cd36fe24
Ivan Yastrebov <esim.i2p@gmail.com>
2021-12-11 10:45:52 +0300

    Sat Dec 11 10:45:52 MSK 2021 Android

---
+++ run_once_after_20_fish_abbr.sh
@@ -0,0 +1,15 @@
+#!/data/data/com.termux/files/usr/bin/env fish
+
+for abbreviation in (abbr -l)
+    abbr -e $abbreviation
+end
+abbr g 'git '
+abbr gf 'git fetch '
+abbr gs 'git status '
+abbr gc 'git commit -m '
+abbr md 'mkdir -p '
+abbr pi '.venv/bin/python -m pip install -U '
+abbr rm 'rm -Rf '
+abbr xx 'chmod +x '
+abbr z 'chezmoi '
+abbr ze 'chezmoi edit --apply '
