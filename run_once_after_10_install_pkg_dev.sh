9aa9665f0932441aa4eebaae915950d817f05e8a
Ivan Yastrebov <esim.i2p@gmail.com>
2021-12-18 07:22:31 +0300

    Android, Sat Dec 18 07:22:31 MSK 2021

--- run_once_after_10_install_pkg_dev.sh
+++ run_once_after_10_install_pkg_dev.sh
@@ -9,6 +9,8 @@ pkg up  -y
 
 pkg i termux-api  git -y
 
+pkg install build-essential cmake python fish -y
+
 pkg i libgmp libmpc libmpfr libxml2 libxslt freetype libjpeg-turbo libpng libsodium libzmq -y
 
 termux-change-repo
